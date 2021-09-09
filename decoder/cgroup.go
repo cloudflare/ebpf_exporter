164dd5a91ec1dc71a5b3b00b46fbceb25576bbce
Andrey Klimentyev <andrey.klimentyev@flant.com>
2021-09-09 10:08:44 +0300

    Scan whole /sys/fs/cgroup directory in the cgroup decoder
    This resolves a problem when you are using a Hybrid systemd cgroup mode
    
    Reference: https://systemd.io/CGROUP_DELEGATION/

--- decoder/cgroup.go
+++ decoder/cgroup.go
@@ -18,7 +18,7 @@ type CGroup struct {
 }
 
 // Decode transforms cgroup id to path in cgroupfs
-func (c *CGroup) Decode(in []byte, conf config.Decoder) ([]byte, error) {
+func (c *CGroup) Decode(in []byte, _ config.Decoder) ([]byte, error) {
 	if c.cache == nil {
 		c.cache = map[uint64][]byte{}
 	}
@@ -46,10 +46,7 @@ func (c *CGroup) Decode(in []byte, conf config.Decoder) ([]byte, error) {
 func (c *CGroup) refreshCache() error {
 	byteOrder := bcc.GetHostByteOrder()
 
-	cgroupPath := "/sys/fs/cgroup/unified"
-	if _, err := os.Stat(cgroupPath); os.IsNotExist(err) {
-		cgroupPath = "/sys/fs/cgroup"
-	}
+	cgroupPath := "/sys/fs/cgroup"
 
 	return filepath.Walk(cgroupPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
