70f05860f50262f01cbf950f92d30348b68533fc
Ivan Babrou <github@ivan.computer>
2022-10-14 16:44:51 -0700

    Compute host byte order once

--- util/util.go
+++ util/util.go
@@ -37,17 +37,27 @@ import (
 	"unsafe"
 )
 
-// GetHostByteOrder returns the current byte-order.
-func GetHostByteOrder() binary.ByteOrder {
+var byteOrder binary.ByteOrder
+
+func init() {
+	byteOrder = initHostByteOrder()
+}
+
+func initHostByteOrder() binary.ByteOrder {
 	buf := [2]byte{}
 	*(*uint16)(unsafe.Pointer(&buf[0])) = uint16(0xAABB)
 
-	if buf[0] == 0xBB{
+	if buf[0] == 0xBB {
 		return binary.LittleEndian
 	}
+
 	return binary.BigEndian
 }
 
+// GetHostByteOrder returns the current byte-order.
+func GetHostByteOrder() binary.ByteOrder {
+	return byteOrder
+}
 
 func ScanFdInfo(fd int, fields map[string]interface{}) error {
 	fdinfo, err := os.Open(fmt.Sprintf("/proc/self/fdinfo/%d", fd))
