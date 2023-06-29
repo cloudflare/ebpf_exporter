ec1e791f2a61feed7ee86b86d91155e227331765
Ivan Babrou <github@ivan.computer>
2023-06-28 23:10:29 -0700

    Simplify xdp example and reduce cardinality

--- examples/xdp.bpf.c
+++ examples/xdp.bpf.c
@@ -3,119 +3,108 @@
 #include <bpf/bpf_endian.h>
 #include "maps.bpf.h"
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
-
 #define ETH_P_IPV6 0x86DD
 #define ETH_P_IP 0x0800
-#define ETH_P_ARP 0x0806
 
-struct tcp_udp_out
-{
-  u32 src_ip;
-  u32 dst_ip;
-  u32 src_port;
-  u32 dst_port;
-  u32 protocol;
+struct packet_key_t {
+    u16 eth_type;
+    u16 proto;
+    u16 port;
 };
 
-struct icmp_out
-{
-  u32 src_ip;
-  u32 dst_ip;
-  u32 type;
-  u32 code;
-  u32 protocol;
+struct hdr_cursor {
+	void *pos;
 };
 
-struct
-{
-  __uint(type, BPF_MAP_TYPE_HASH);
-  __uint(max_entries, 10000);
-  __type(key, struct tcp_udp_out);
-  __type(value, u64);
-} xdp_tcp_map SEC(".maps");
+struct {
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
+    __uint(max_entries, 1024);
+    __type(key, struct packet_key_t);
+    __type(value, u64);
+} xdp_incoming_packets_total SEC(".maps");
 
-struct
-{
-  __uint(type, BPF_MAP_TYPE_HASH);
-  __uint(max_entries, 10000);
-  __type(key, struct tcp_udp_out);
-  __type(value, u64);
-} xdp_udp_map SEC(".maps");
-
-struct
-{
-  __uint(type, BPF_MAP_TYPE_HASH);
-  __uint(max_entries, 10000);
-  __type(key, struct icmp_out);
-  __type(value, u64);
-} xdp_icmp_map SEC(".maps");
-
-struct
-{
-  __uint(type, BPF_MAP_TYPE_HASH);
-  __uint(max_entries, 10000);
-  __type(key, u32);
-  __type(value, u64);
-} xdp_total_packets_map SEC(".maps");
-
-SEC("xdp/enp8s0,lo")
-int xdp_trace(struct xdp_md *ctx)
-{
-  void *data_end = (void *)(long)ctx->data_end;
-  void *data = (void *)(long)ctx->data;
-  struct ethhdr *eth = (struct ethhdr *)(data);
-  if (eth + 1 > (struct ethhdr *)data_end)
-    return XDP_PASS;
-  u32 c = 0;
-  increment_map(&xdp_total_packets_map, &c, 1);
-  if (eth->h_proto == bpf_htons(ETH_P_IP))
-  {
-    struct iphdr *iph = data + sizeof(*eth);
-    if (iph + 1 > (struct iphdr *)data_end)
-      return XDP_PASS;
-    if (iph->protocol == IPPROTO_UDP)
-    {
-      struct udphdr *udph = data + sizeof(*eth) + sizeof(*iph);
-      if (udph + 1 > (struct udphdr *)data_end)
-        return XDP_PASS;
-      struct tcp_udp_out output = {
-          .src_ip = iph->saddr,
-          .dst_ip = iph->daddr,
-          .src_port = bpf_ntohs(udph->source),
-          .dst_port = bpf_ntohs(udph->dest),
-          .protocol = IPPROTO_UDP,
-      };
-      increment_map(&xdp_udp_map, &output, 1);
-    }
-    else if (iph->protocol == IPPROTO_TCP)
-    {
-      struct tcphdr *tcph = data + sizeof(*eth) + sizeof(*iph);
-      if (tcph + 1 > (struct tcphdr *)data_end)
-        return XDP_PASS;
-      struct tcp_udp_out output = {
-          .src_ip = iph->saddr,
-          .dst_ip = iph->daddr,
-          .src_port = bpf_ntohs(tcph->source),
-          .dst_port = bpf_ntohs(tcph->dest),
-          .protocol = IPPROTO_TCP,
-      };
-      increment_map(&xdp_tcp_map, &output, 1);
-    }
-    else if (iph->protocol == IPPROTO_ICMP)
-    {
-      struct icmphdr *icmph = data + sizeof(*eth) + sizeof(*iph);
-      if (icmph + 1 > (struct icmphdr *)data_end)
-        return XDP_PASS;
-      struct icmp_out output = {
-          .src_ip = iph->saddr,
-          .dst_ip = iph->daddr,
-          .type = icmph->type,
-          .code = icmph->code,
-          .protocol = IPPROTO_ICMP,
-      };
-      increment_map(&xdp_icmp_map, &output, 1);
-    }
-  }
-  return XDP_PASS;
+// Primitive header extraction macros. See xdp-tutorial repo for more robust parsers:
+// * https://github.com/xdp-project/xdp-tutorial/blob/master/common/parsing_helpers.h
+#define parse_args struct hdr_cursor *cursor, void *data_end, struct
+#define parse_header(type) static bool parse_##type(parse_args type **hdr) { \
+    size_t offset = sizeof(**hdr);                                           \
+                                                                             \
+    if (cursor->pos + offset > data_end) {                                   \
+        return false;                                                        \
+    }                                                                        \
+                                                                             \
+    *hdr = cursor->pos;                                                      \
+    cursor->pos += offset;                                                   \
+                                                                             \
+    return true;                                                             \
 }
+
+parse_header(ethhdr)
+parse_header(iphdr)
+parse_header(ipv6hdr)
+parse_header(tcphdr)
+parse_header(udphdr)
+
+SEC("xdp/lo")
+int xdp_trace(struct xdp_md *ctx) {
+    void *data_end = (void *) (long) ctx->data_end;
+    void *data = (void *) (long) ctx->data;
+    struct packet_key_t key = {};
+    struct hdr_cursor cursor = { .pos = data };
+    struct ethhdr *eth_hdr;
+    struct iphdr *ip_hdr;
+    struct ipv6hdr *ipv6_hdr;
+    struct udphdr *udp_hdr;
+    struct tcphdr *tcp_hdr;
+
+    if (!parse_ethhdr(&cursor, data_end, &eth_hdr)) {
+        return XDP_PASS;
+    }
+
+    key.eth_type = bpf_ntohs(eth_hdr->h_proto);
+
+    switch (eth_hdr->h_proto) {
+    case bpf_htons(ETH_P_IP):
+        if (!parse_iphdr(&cursor, data_end, &ip_hdr)) {
+            return XDP_PASS;
+        }
+
+        key.proto = ip_hdr->protocol;
+        break;
+    case bpf_htons(ETH_P_IPV6):
+        if (!parse_ipv6hdr(&cursor, data_end, &ipv6_hdr)) {
+            return XDP_PASS;
+        }
+
+        key.proto = ipv6_hdr->nexthdr;
+        break;
+    }
+
+    switch (key.proto) {
+    case IPPROTO_TCP:
+        if (!parse_tcphdr(&cursor, data_end, &tcp_hdr)) {
+            return XDP_PASS;
+        }
+
+        key.port = bpf_ntohs(tcp_hdr->dest);
+        break;
+    case IPPROTO_UDP:
+        if (!parse_udphdr(&cursor, data_end, &udp_hdr)) {
+            return XDP_PASS;
+        }
+
+        key.port = bpf_ntohs(udp_hdr->dest);
+        break;
+    }
+
+    // Skip ephemeral port range to keep metrics tidy
+    if (key.port >= 32768) {
+        return XDP_PASS;
+    }
+
+    increment_map(&xdp_incoming_packets_total, &key, 1);
+
+    return XDP_PASS;
+}
+
+char LICENSE[] SEC("license") = "GPL";
