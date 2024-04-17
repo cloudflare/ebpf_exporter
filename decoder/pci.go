8587c95ebc38449a236ca3d627b9a075e0ef6e4e
Ivan Babrou <github@ivan.computer>
2024-04-16 21:48:36 -0700

    Probe /usr/share/hwdata/pci.ids for pci.ids db
    This largely addresses #377.

--- decoder/pci.go
+++ decoder/pci.go
@@ -3,25 +3,31 @@ package decoder
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jaypipes/pcidb"
 )
 
-const pciIDsPath = "/usr/share/misc/pci.ids"
 const missingPciIDsText = "missing pci.ids db"
 
+var pciIDsPaths = []string{"/usr/share/misc/pci.ids", "/usr/share/hwdata/pci.ids"}
 var pci *pcidb.PCIDB
 
 func init() {
-	if _, err := os.Stat(pciIDsPath); err != nil {
-		log.Printf("PCI DB path %q is not accessible: %v", pciIDsPath, err)
+	for _, path := range pciIDsPaths {
+		if _, err := os.Stat(path); err != nil {
+			continue
+		}
+
+		db, err := pcidb.New()
+		if err != nil {
+			log.Fatalf("Error initializing PCI DB: %v", err)
+		}
+
+		pci = db
+
 		return
 	}
 
-	db, err := pcidb.New()
-	if err != nil {
-		log.Fatalf("Error initializing PCI DB: %v", err)
-	}
-
-	pci = db
+	log.Printf("None of the PCI DB paths (%s) are accessible, PCI decoders will return empty data", strings.Join(pciIDsPaths, ", "))
 }
