c891328833f954f082e92067f4df8758de4816dc
Jesper Dangaard Brouer <hawk@kernel.org>
2025-08-20 13:11:15 +0200

    Adjust netstacklat example to compile
    Keeping this as seperate commit to track what needed to change
    
    Signed-off-by: Jesper Dangaard Brouer <hawk@kernel.org>

--- examples/netstacklat.bpf.c
+++ examples/netstacklat.bpf.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
-#include "vmlinux_local.h"
-#include <linux/bpf.h>
+#include <vmlinux.h>
+//#include <linux/bpf.h>
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
